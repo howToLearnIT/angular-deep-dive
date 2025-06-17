9e70d1fada1fc6aa4cfd0eb0eaea1f4279681253
howToLearnIT <fifarange16@gmail.com>
2025-06-17 20:40:34 +0300

    1.2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,40 +1,5 @@
 import { Component } from '@angular/core';
-
-class Subject {
-    private observers: Array<(data: string) => void> = [];
-
-    subscribe(observer: (data: string) => void) {
-        this.observers.push(observer);
-    }
-
-    unsubscribe(observer: (data: string) => void) {
-        this.observers = 
-          this.observers.filter((currenctObserver) => currenctObserver !== observer);
-    }
-
-    notify(data: string) {
-        this.observers.forEach(observer => observer(data));
-    }
-}
-
-// Использование
-const subject = new Subject();
-
-const observer1 = (data: string) => console.log(`Наблюдатель 1: ${data}`);
-const observer2 = (data: string) => console.log(`Наблюдатель 2: ${data}`);
-
-subject.subscribe(observer1);
-subject.subscribe(observer2);
-
-subject.notify("Привет!"); 
-
-// Вывод:
-// Наблюдатель 1: Привет!
-// Наблюдатель 2: Привет!
-
-subject.unsubscribe(observer1);
-subject.notify("Мир!"); 
-// Вывод: Наблюдатель 2: Мир!
+import { fromEvent, map, scan, throttleTime } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -43,4 +8,66 @@ subject.notify("Мир!");
   `,
   imports: [],
 })
-export class App {}
+export class App {
+
+  constructor() {
+    this.example1();
+    // this.example2();
+    // this.example3();
+    // this.example4();
+  }
+
+  example1() {
+    // document.addEventListener('click', () => console.log('Clicked!'));
+    fromEvent(document, 'click').subscribe(() => console.log('Clicked!'));
+  }
+
+  example2() {
+    // let count = 0;
+    // document.addEventListener('click', () => console.log(`Clicked ${++count} times`));
+
+    fromEvent(document, 'click')
+      .pipe(scan((count) => count + 1, 0))
+      .subscribe((count) => console.log(`Clicked ${count} times`));
+  }
+
+  example3() {
+    // let count = 0;
+    // let rate = 1000;
+    // let lastClick = Date.now() - rate;
+    // document.addEventListener('click', () => {
+    //   if (Date.now() - lastClick >= rate) {
+    //     console.log(`Clicked ${++count} times`);
+    //     lastClick = Date.now();
+    //   }
+    // });
+  
+    fromEvent(document, 'click')
+      .pipe(
+        throttleTime(1000),
+        scan((count) => count + 1, 0)
+      )
+      .subscribe((count) => console.log(`Clicked ${count} times`));
+  }
+
+  example4() {
+    // let count = 0;
+    // const rate = 1000;
+    // let lastClick = Date.now() - rate;
+    // document.addEventListener('click', (event) => {
+    //   if (Date.now() - lastClick >= rate) {
+    //     count += event.clientX;
+    //     console.log(count);
+    //     lastClick = Date.now();
+    //   }
+    // });
+  
+    fromEvent(document, 'click')
+      .pipe(
+        throttleTime(1000),
+        map((event) => (event as MouseEvent).clientX),
+        scan((count, clientX) => count + clientX, 0)
+      )
+      .subscribe((count) => console.log(count));
+      }
+}
