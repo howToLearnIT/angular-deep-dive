45fb54ecd2580a436908c9ce8390405132528ca2
howToLearnIT <fifarange16@gmail.com>
2025-06-18 19:54:17 +0300

    1.7

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { catchError, map, of, retry } from 'rxjs';
+import { concatMap, from, groupBy, map, mergeMap, of, pairwise, scan, switchMap, toArray } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,78 +9,75 @@ import { catchError, map, of, retry } from 'rxjs';
   imports: [],
 })
 export class App {
+	constructor() {
+		this.example1();
+		// this.example2();
+		// this.example3();
+		// this.example4();
+		// this.example5();
+		// this.example6();
+		// this.example7();
+	}
 
-  constructor() {
-    this.example1();
-    // this.example2();
-    // this.example3();
-    // this.example4();
-  }
+	example1() {
+		of(1, 2, 3, 4, 5)
+			.pipe(
+				map(count => count * count),
+			)
+			.subscribe(result => console.log(result));
+	}
 
-  example1() {
-    of(1, 2, 3, 4, 5)
-      .pipe(
-        map(n => {
-          if (n === 4) {
-            throw 'four!';
-          }
-          return n;
-        }),
-        catchError(err => of('I', 'II', 'III', 'IV', 'V'))
-      )
-      .subscribe(x => console.log(x));
-  }
+	example2() {
+		of(1, 2, 3)
+			.pipe(
+				scan((total, n) => total + n, 0),
+			)
+			.subscribe(result => console.log(result));
+  	}
 
-  example2() {
-    of(1, 2, 3, 4, 5)
-      .pipe(
-        map(n => {
-          if (n === 4) {
-            throw 'four!';
-          }
-          return n;
-        }),
-        catchError((err, caught) => {
-          return caught
-        }),
-      )
-      .subscribe(x => console.log(x));
-  } 
+	example3() {
+		from([1, 2, 3])
+			.pipe(
+				mergeMap(id => fetch(`https://pokeapi.co/api/v2/pokemon/ditto?id=${id}`))
+			)
+			.subscribe(post => console.log(post));
+  	}
 
-  example3() {
-    of(1, 2, 3, 4, 5)
-      .pipe(
-        map(n => {
-          if (n === 4) {
-            throw 'four!';
-          }
-          return n;
-        }),
-        retry(3)
-      )
-      .subscribe({
-        next: (data) => console.log('Успех:', data),
-        error: (err) => console.error('Ошибка после 3 попыток:', err),
-      });
-  } 
+	example4() {
+		from([1, 2, 3])
+			.pipe(
+				concatMap(id => fetch(`https://pokeapi.co/api/v2/pokemon/ditto?id=${id}`))
+			)
+			.subscribe(post => console.log(post));
+  	}
 
-  example4() {
-    of(1, 2, 3, 4, 5)
-      .pipe(
-        map(n => {
-          if (n === 4) {
-            throw 'four!';
-          }
-          return n;
-        }),
-        retry({
-          count: 3,
-          delay: 1000, // Фиксированная задержка 1 сек
-        })
-      )
-      .subscribe({
-        next: (data) => console.log('Успех:', data),
-        error: (err) => console.error('Ошибка после 3 попыток:', err),
-      });
-  }
+	example5() {
+		from([1, 2, 3])
+			.pipe(
+				switchMap(id => fetch(`https://pokeapi.co/api/v2/pokemon/ditto?id=${id}`))
+			)
+			.subscribe(post => console.log(post));
+  	}
+
+	example6() {
+		of(
+			{ id: 1, name: 'JavaScript' },
+			{ id: 2, name: 'Parcel' },
+			{ id: 2, name: 'webpack' },
+			{ id: 1, name: 'TypeScript' },
+			{ id: 3, name: 'TSLint' }
+		).pipe(
+			groupBy(p => p.id),
+			mergeMap(group => group.pipe(toArray()))   
+		)
+		.subscribe(p => console.log(p));
+  	}
+
+	example7() {
+		of(1, 2, 3, 4, 5)
+			.pipe(
+				pairwise()
+			)
+			.subscribe(pair => console.log(pair));
+  	}
 }
