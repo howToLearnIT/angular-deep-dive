6336475c03dc5572d9346d618dac729c147d2168
howToLearnIT <fifarange16@gmail.com>
2025-06-18 16:52:23 +0300

    1.5

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { from, fromEvent, interval, of, range, timer } from 'rxjs';
+import { delay, from, fromEvent, interval, of, range, tap, timer, timestamp, toArray } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,39 +15,33 @@ export class App {
     // this.example2();
     // this.example3();
     // this.example4();
-    // this.example5();
-    // this.example6();
   }
 
   example1() {
-    const array = [10, 20, 30];
-    const result = from(array);
-
-    result.subscribe(x => console.log(x));
+    of(Math.random()).pipe(
+      tap((value)=> console.log('Я побочный эффект ',value)),
+    ).subscribe(console.log);
   }
 
   example2() {
-    const promise = fetch('https://api.github.com/users?per_page=5');
-    from(promise).subscribe(data => console.log(data));
+    fromEvent(document, 'click')
+      .pipe(
+        delay(1000)
+      )
+      .subscribe(event => console.log(event));
   } 
 
   example3() {
-    fromEvent(document, 'click').subscribe(event => console.log(event));
+    fromEvent(document, 'click')
+      .pipe(
+        timestamp()
+      )
+      .subscribe(event => console.log(event));
   } 
 
   example4() {
-    timer(1000).subscribe(data => console.log(data));
-  }
-
-  example5() {
-    interval(1000).subscribe(count => console.log(count));
-  }
-
-  example6() {
-    range(1, 3).subscribe(x => console.log(x));
-  }
-
-  example7() {
-    of(10, 20).subscribe(x => console.log(x));
+    of(1, 2, 3, 4, 5).pipe(
+      toArray()
+    ).subscribe(console.log);
   }
 }
