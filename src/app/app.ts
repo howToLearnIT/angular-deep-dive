bd6a1475922433ec35f278dd26c389e53c748ece
howToLearnIT <fifarange16@gmail.com>
2025-06-21 22:08:42 +0300

    2.4

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,60 +1,40 @@
 import { Component, effect, linkedSignal, Signal, signal, untracked, WritableSignal } from '@angular/core';
-import { timer } from 'rxjs';
+import { BehaviorSubject, interval, timer } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
-  template: `
-      Смотри консоль
-  `,
+  template: `Смотри в консоль`,
   imports: [],
 })
 export class App {
-	options: WritableSignal<number[]> = signal([1, 2, 3, 4, 5]);
-
-	selectedOption = signal(this.options()[0]);
-	// selectedOption = linkedSignal(() => this.options()[0]);
-
 	constructor() {
-		console.log('Выбранная опция ', this.selectedOption())
+		// this.example1();
+		this.example2();
+	}
 
+	example1() {
+		const counterObservable$ = interval(100);
+
+		const counter = toSignal(counterObservable$, {initialValue: 0});
+
+		console.log('Счетчик ', counter())
+		
 		timer(1000).subscribe(()=> {
-			this.changeOption(2);
-			console.log('Выбранная опция ', this.selectedOption())
+			console.log('Счетчик через секунду ', counter())
 		})
-
-		timer(3000).subscribe(()=> {
-			this.options.set([5, 4, 3, 2, 1])
-			console.log('Выбранная опция ', this.selectedOption())
-		})
-
-		// this.exampleEqual();
 	}
 
-	changeOption(newOptionIndex: number) {
-		this.selectedOption.set(this.options()[newOptionIndex]);
+	example2() {
+		const subject$ = new BehaviorSubject<number>(0);
+
+		const counter = toSignal(subject$);
+
+		console.log('Счетчик ', counter())
+		
+		subject$.next(1);
+		
+		console.log('Счетчик ', counter())
 	}
 
-	exampleEqual() {
-		const activeUser = signal({id: 123, name: 'Morgan', isAdmin: true});
-
-		const activeUserEditCopy = linkedSignal(() => activeUser(), {
-			equal: (a, b) => a.id === b.id,
-		});
-
-		// const activeUserEditCopy = linkedSignal({
-		// 	source: activeUser,
-		// 	computation: user => user,
-		// 	equal: (a, b) => a.id === b.id,
-		// });
-
-		console.log('Юзер с возможностью редактирования ', activeUserEditCopy());
-
-		activeUser.set({id: 123, name: 'NOT Morgan', isAdmin: false})
-
-		console.log('Юзер с возможностью редактирования ', activeUserEditCopy());
-
-		// activeUser.set({id: 1, name: 'NOT Morgan', isAdmin: false})
-
-		// console.log('Юзер с возможностью редактирования ', activeUserEditCopy());
-	}
 }
