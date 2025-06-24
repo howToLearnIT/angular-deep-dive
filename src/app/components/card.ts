0437e1b1856bb0e2457716bdb33f8aa2c297cfd9
howToLearnIT <fifarange16@gmail.com>
2025-06-24 20:20:26 +0300

    3.5

---
+++ src/app/components/card.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, output } from "@angular/core";
+import { interval, timeout, timer } from "rxjs";
+
+@Component({
+    selector: 'yt-card',
+    template: `
+        Панель
+    `,
+})
+export class Card {
+
+}
