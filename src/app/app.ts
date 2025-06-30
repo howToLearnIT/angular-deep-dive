2ad56a46e9d8c0bd1df9af042265b1cea16b3c28
howToLearnIT <fifarange16@gmail.com>
2025-06-30 20:26:33 +0300

    5.2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-import { MyComponent } from './components/component';
-import { MyDirective } from './directives/directive';
+import { Highlight1 } from './directives/highlight-1';
+import { Highlight2 } from './directives/highlight-2';
+import { Highlight3 } from './directives/highlight-3';
+import { NgClass } from '@angular/common';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.html',
 	styleUrl: './app.css',
-	imports: [MyComponent, MyDirective],
+	imports: [Highlight1, Highlight2, Highlight3, NgClass],
 })
-export class App {}
+export class App {
+	isSpecial = true;
+}
