ef13a00298d39d63b7d1ce7e473a02f18e30659e
howToLearnIT <fifarange16@gmail.com>
2025-07-07 12:26:37 +0300

    7.2

--- src/app/pages/home.page.ts
+++ src/app/pages/home.page.ts
@@ -1,7 +1,12 @@
 import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 @Component({
     selector: 'home-page',
-    template: 'Домашняя страница',
+    template: `
+        Домашняя страница <br/>
+        <a routerLink="admin"> В админку</a>
+    `,
+    imports: [RouterLink]
 })
 export class HomePage {}
